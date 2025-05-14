a1cc836aa9d5b9ce72e1ee15d4c94bcc62c37e2f
Rummonislam786 <islam.rummon786@gmail.com>
2025-05-15 04:53:39 +0600

    start from the beginning now we are here

---
+++ admin/db_connect.php
@@ -0,0 +1,6 @@
+<?php 
+
+$conn1= new mysqli('localhost','root','','fos_cheezy_v2')or die("Could not connect to mysql".mysqli_error($conn1));
+$conn2= new mysqli('localhost','root','','fos_cheezy_v2')or die("Could not connect to mysql".mysqli_error($conn1));
+$conn3= new mysqli('localhost','root','','fos_cheezy_v2')or die("Could not connect to mysql".mysqli_error($conn1));
+$conn4= new mysqli('localhost','root','','fos_cheezy_v2')or die("Could not connect to mysql".mysqli_error($conn1));
